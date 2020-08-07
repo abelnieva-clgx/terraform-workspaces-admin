a902d031a8c209d1871e032529cb2d8ca1ec57ef
Abel Alejandro Nieva <Abel_Alejandro_Nieva@epam.com>
2020-08-07 02:32:41 +0200

    fixed name directory

--- workspace-demo_app-dev.tf
+++ workspace-demo_app-dev.tf
@@ -2,7 +2,7 @@ resource "tfe_workspace" "demo_app-dev" {
   name              = "demo_app-dev"
   organization      = tfe_organization.exampleOrg.id
   terraform_version = var.tfe12_version
-  working_directory = demo_app-dev
+  working_directory = "demo_app-dev"
   vcs_repo {
     identifier     = "abelnieva-clgx/terraform-demo-apps"
     oauth_token_id = tfe_oauth_client.github.oauth_token_id 
--- workspace-demo_app-prod.tf
+++ workspace-demo_app-prod.tf
@@ -2,7 +2,7 @@ resource "tfe_workspace" "demo_app-prod" {
   name              = "demo_app-prod"
   organization      = tfe_organization.exampleOrg.id
   terraform_version = var.tfe12_version
-  working_directory = demo_app-prod
+  working_directory = "demo_app-prod"
   vcs_repo {
     identifier     = "abelnieva-clgx/terraform-demo-apps"
     oauth_token_id = tfe_oauth_client.github.oauth_token_id 
